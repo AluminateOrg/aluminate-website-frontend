ca00de7055a4325a76047fdd44fbf2ac09ef81fb
senira277 <senira277@gmail.com>
2025-10-12 15:48:47 +0530

    s

--- components/animation/CountUp.tsx
+++ components/animation/CountUp.tsx
@@ -36,12 +36,8 @@ export default function CountUp({
   const damping = 20 + 40 * (1 / duration);
   const stiffness = 100 * (1 / duration);
 
-  const springValue = useSpring(motionValue, {
-    damping,
-    stiffness,
-  });
+  const springValue = useSpring(motionValue, { damping, stiffness });
 
-  // Fix: Cast the ref to satisfy the useInView type
   const isInView = useInView(ref as React.RefObject<Element>, { once: true, margin: "0px" });
 
   useEffect(() => {
@@ -52,50 +48,29 @@ export default function CountUp({
 
   useEffect(() => {
     if (isInView && startWhen) {
-      if (typeof onStart === "function") {
-        onStart();
-      }
+      onStart?.();
 
       const timeoutId = setTimeout(() => {
         motionValue.set(direction === "down" ? from : to);
       }, delay * 1000);
 
-      const durationTimeoutId = setTimeout(() => {
-        if (typeof onEnd === "function") {
-          onEnd();
-        }
-      }, delay * 1000 + duration * 1000);
+      const durationTimeoutId = setTimeout(() => onEnd?.(), delay * 1000 + duration * 1000);
 
       return () => {
         clearTimeout(timeoutId);
         clearTimeout(durationTimeoutId);
       };
     }
-  }, [
-    isInView,
-    startWhen,
-    motionValue,
-    direction,
-    from,
-    to,
-    delay,
-    onStart,
-    onEnd,
-    duration,
-  ]);
+  }, [isInView, startWhen, motionValue, direction, from, to, delay, onStart, onEnd, duration]);
 
   useEffect(() => {
     const unsubscribe = springValue.on("change", (latest) => {
       if (ref.current) {
-        const options = {
+        const formattedNumber = Intl.NumberFormat("en-US", {
           useGrouping: !!separator,
           minimumFractionDigits: 0,
           maximumFractionDigits: 0,
-        };
-
-        const formattedNumber = Intl.NumberFormat("en-US", options).format(
-          Number(latest.toFixed(0))
-        );
+        }).format(Number(latest.toFixed(0)));
 
         ref.current.textContent = separator
           ? formattedNumber.replace(/,/g, separator)
@@ -106,5 +81,5 @@ export default function CountUp({
     return () => unsubscribe();
   }, [springValue, separator]);
 
-  return <span className={`${className}`} ref={ref} />;
+  return <span className={className} ref={ref} />;
 }
